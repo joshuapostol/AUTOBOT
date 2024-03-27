80ef971ec918b0294e78edcafd2f02060ae65fab
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-03-27 14:13:10 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,44 +1,38 @@
-   const axios = require('axios');
+const axios = require('axios');
 
-   module.exports.config = {
-     name: "ai",
-     version: "1.0.0",
-     hasPermssion: 0,
-     credits: "churchill abing",
-     description: "https://www.facebook.com/profile.php?id=100087212564100",
-     usePrefix: false,
-     commandCategory: "ai",
-     usages: "[question]",
-     cooldowns: 5,
-     dependencies: {
-       "axios": ""
-     }
-   };
+module.exports.config = {
+  name: "ai",
+  version: "69",
+  role: 0,
+  credits: "🄼🄾🄳🄸🄵🄸🄴🄳 🄱🅈 🄲🄷🅄🅁🄲🄷🄸🄻🄻", // converted by kira
+  description: "ask AI",
+  usages: "ask <question>",
+  hasPrefix: false,
+  commandCategory: "ai",
+  cooldowns: 0
+};
 
-module.exports.handleEvent = async function ({ api, event }) {
-     const lowerBody = event.body.toLowerCase();
-     if (!(lowerBody.startsWith("eurix") || lowerBody.startsWith("ai"))) return;
+module.exports.run = async function ({ api, event, args, message }) {
+  try {
+    const prompt = event.body.trim();
+    if (!prompt) {
+      await api.sendMessage({ body: "Hey I am Ai, ask me questions dear 🤖" }, event.threadID);
+      return;
+    }
+    api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
+    const response = await axios.get(`https://sandipapi.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+    const answer = response.data.answer;
 
-     const args = event.body.split(/\s+/);
-     args.shift();
+    await api.sendMessage({
+      body: `𝗕𝗢𝗧 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 | 🟢
+━━━━━━━━━━━━━━━━━━        
+${answer}
+━━━━━━━━━━━━━━━━━━\n\n- 𝚃𝚑𝚒𝚜 𝚋𝚘𝚝 𝚞𝚗𝚍𝚎𝚛 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝚋𝚢 churchill\n• 𝐅𝐛𝐥𝐢𝐧𝐤: >>https://www.facebook.com/profile.php?id=100087212564100,
+    }, event.threadID);
 
-     const question = args.join(" ");
-     if (!question) {
-       api.sendMessage("please provide your question", event.threadID, event.messageID);
-       return;
-     }
-
-     api.sendMessage(`Answering your question...`, event.threadID, event.messageID);
-
-     const apiUrl = `https://eurix-api.replit.app/gpt4?ask=${encodeURIComponent(question)}`;
-
-     try {
-       const response = await axios.get(apiUrl);
-       const answer = response.data.answer;
-
-       api.sendMessage(answer, event.threadID, event.messageID);
-     } catch (error) {
-       console.error("Error fetching AI response:", error);
-       api.sendMessage("Error fetching AI response", event.threadID, event.messageID);
-     }
-   };
+  } catch (error) {
+    console.error("🔴 An error occurred while processing your request.\nPlease contact Jay Mar for an error", error.message);
+    api.setMessageReaction("🔴", event.messageID, (err) => {}, true);
+  }
+};
