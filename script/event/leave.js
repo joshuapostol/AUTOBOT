cc224dbf983bc66de6960cbf1e19b9572aae9139
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-03-28 14:46:42 +0800

    Create leave.js

---
+++ script/event/leave.js
@@ -0,0 +1,22 @@
+﻿module.exports.config = {
+	name: "leave",
+	eventType: ["log:unsubscribe"],
+	version: "1.0.0",
+	credits: "ryuko",
+	description: "notify leave.",
+};
+
+module.exports.run = async function({ api, event, Users, Threads }) {
+	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+	const { threadID } = event;
+	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "ingat sa byahe haha" : "ayan mateluk ka kase haha";
+	var msg, formPush
+	(typeof data.customLeave == "undefined") ? msg = "fly high {name}, {type}" : msg = data.customLeave;
+	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+
+	var formPush = { body: msg }
+	
+	return api.sendMessage(formPush, threadID);
+}
