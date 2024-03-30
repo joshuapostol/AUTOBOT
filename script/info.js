66b97df95f247f32b1b8827d0bf62665a2f6af17
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-03-30 22:37:41 +0800

    Update info.js

--- script/info.js
+++ script/info.js
@@ -28,7 +28,7 @@ module.exports.run = async function({ api, event, args }) {
 		const CREATORLINK = "https://www.facebook.com/profile.php?id=100087212564100";
 		const BOTCREATOR = "churchill abing";
 		const BOTNAME = "CHURCHILLBOT";
-		const FILESOWNER = "Jay";
+		const FILESOWNER = "chill";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
 "https://i.imgur.com/IGXINCB.mp4",
