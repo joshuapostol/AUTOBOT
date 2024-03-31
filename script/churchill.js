40e4554a0f78e3915006b873dd5ecb345b95386b
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-03-31 09:13:48 +0800

    Update and rename yazky.js to churchill.js

--- script/churchill.js
+++ script/churchill.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports.config = {
- name: "popcat",
+ name: "chill",
  version: ".",
  role: 0,
  aliases: ["pop"],
@@ -16,10 +16,10 @@ module.exports.run = async ({ api, event, args }) => {
  try {
 	let message = args.join(" ");
 	if (!message) {
-	 return api.sendMessage(`please add your message to talk popcat simsimi`, event.threadID, event.messageID);
+	 return api.sendMessage(`please add your message to talk churchillai`, event.threadID, event.messageID);
 	}
 
-	const response = await axios.get(`https://api.popcat.xyz/chatbot?msg=${encodeURIComponent(message)}&owner=Zero+Two&botname=Pop+Cat`);
+	const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(message)}&owner=Zero+Two&botname=Pop+Cat`);
 	const respond = response.data.response;
 	api.sendMessage(respond, event.threadID, event.messageID);
  } catch (error) {
