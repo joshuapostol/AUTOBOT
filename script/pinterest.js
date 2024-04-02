65286b2f5c7b418aa283358bde5aa84e354892bc
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-02 09:47:44 +0800

    Update pinterest.js

--- script/pinterest.js
+++ script/pinterest.js
@@ -1,47 +1,37 @@
-const axios = require("axios");
-const fs = require("fs");
-async function pinterest(event, api) {
-  let input = event.body.split(" ");
-  if (input.length < 2) {
-    api.sendMessage("Missing input!", event.threadID, event.messageID);
-  } else {
-    try {
-      input.shift();
-      let txt = input.join(" ");
-      const res = await axios.get(
-        `https://api-all-1.arjhilbard.repl.co/pinterest?search=${encodeURIComponent(
-          txt,
-        )}`,
-      );
-      const data = res.data.data;
-      var num = 0;
-      var imgData = [];
-      for (var i = 0; i < 12; i++) {
-        let getDown = (
-          await axios.get(`${data[i]}`, { responseType: "arraybuffer" })
-        ).data;
-        fs.writeFileSync(
-          `${__dirname}/../temp/pinterest-${(num += 1)}.jpg`,
-          Buffer.from(getDown, "utf-8"),
-        );
-        imgData.push(fs.createReadStream(`${__dirname}/../temp/pinterest-${num}.jpg`));
-      }
-      api.sendMessage(
-        {
-          attachment: imgData,
-          body: "Here's your images!",
-        },
-        event.threadID,
-        event.messageID,
-      );
-      /*
-      for (let ii = 1; ii < 12; ii++) {
-        fs.unlinkSync(`${__dirname}/../temp/pinterest-${ii}.jpg`);
-      }
-      */
-    } catch (err) {
-      api.sendMessage(`${err}`, event.threadID, event.messageID);
+module.exports.config = {
+    name: "pinterest",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Joshua Sy",
+    description: "Image search",
+    usePrefix: true,
+    commandCategory: "Search",
+    usages: "[Text]",
+    cooldowns: 0,
+};
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const request = require("request");
+    const keySearch = args.join(" ");
+    if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pinterest Naruto - 10 (it depends on you how many images you want to appear in the result)', event.threadID, event.messageID)
+    const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
+    const numberSearch = keySearch.split("-").pop() || 6
+    const res = await axios.get(`https://api-dien.kira1011.repl.co/pinterest?search=${encodeURIComponent(keySearchs)}`);
+    const data = res.data.data;
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
-  }
-}
-module.exports = pinterest;
+    api.sendMessage({
+        attachment: imgData,
+        body: numberSearch + 'Search results for keyword: '+ keySearchs
+    }, event.threadID, event.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    }
+};
