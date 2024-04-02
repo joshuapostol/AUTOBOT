95fcd3a3f61940fc74ade192ec75d07ba47ee8a9
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-02 22:43:22 +0800

    Update callad.js

--- script/callad.js
+++ script/callad.js
@@ -4,32 +4,29 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "NTKhang, ManhG Fix Get",
 	description: "Report bot's error to admin or comment",
+	usePrefix: true,
+	commandCategory: "report",
 	usages: "[Error encountered or comments]",
-	cooldown: 5,
-	hasPrefix: false,
+	cooldowns: 5
 };
 
-module.exports.handleReply = async function({ api: e, args: n, event: a, Users: s, handleReply: o, prefix: t }) {
+module.exports.handleReply = async function({
+	api: e,
+	args: n,
+	event: a,
+	Users: s,
+	handleReply: o
+}) {
 	var i = await s.getNameUser(a.senderID);
 	switch (o.type) {
 		case "reply":
-			var admins = this.config.ADMINBOT; // Assuming ADMINBOT is defined elsewhere
-			for (let admin of admins) {
-				e.sendMessage({
-					body: "📄Feedback from " + i + ":\n" + a.body,
-					mentions: [{
-						id: a.senderID,
-						tag: i
-					}]
-				}, admin, ((e, n) => global.client.handleReply.push({
-					name: this.config.name,
-					messageID: n.messageID,
-					messID: a.messageID,
-					credits: a.senderID,
-					id: a.threadID,
-					type: "calladmin"
-				})));
-			}
+			e.sendMessage({
+				body: "📄Feedback from " + i + ":\n" + a.body,
+				mentions: [{
+					id: a.senderID,
+					tag: i
+				}]
+			}, o.id);
 			break;
 		case "calladmin":
 			e.sendMessage({
@@ -38,35 +35,32 @@ module.exports.handleReply = async function({ api: e, args: n, event: a, Users: 
 					tag: i,
 					id: a.senderID
 				}]
-			}, o.id, ((e, n) => global.client.handleReply.push({
-				name: this.config.name,
-				credits: a.senderID,
-				messageID: n.messageID,
-				type: "reply"
-			})), o.messID);
-			break;
+			}, o.id)
 	}
 };
 
-module.exports.run = async function({ api: e, event: n, args: a, Users: s, Threads: o }) {
+module.exports.run = async function({
+	api: e,
+	event: n,
+	args: a,
+	Users: s,
+	Threads: o
+}) {
 	if (!a[0]) return e.sendMessage("You have not entered the content to report", n.threadID, n.messageID);
 	let i = await s.getNameUser(n.senderID);
 	var t = n.senderID,
 		d = n.threadID;
-	let threadInfo = (await o.getData(n.threadID)).threadInfo;
+	let r = (await o.getData(n.threadID)).threadInfo;
 	var l = require("moment-timezone").tz("Asia/Manila").format("HH:mm:ss D/MM/YYYY");
-	e.sendMessage(`At: ${l}\nYour report has been sent to the bot admins`, n.threadID, (() => {
-		var admins = this.config.ADMINBOT; // Assuming ADMINBOT is defined elsewhere
-		for (let admin of admins) {
-			let threadName = threadInfo.threadName;
-			e.sendMessage(`👤Report from: ${i}\n👨‍👩‍👧‍👧Box: ${threadName}\n🔰ID Box: ${d}\n🔷ID Use: ${t}\n-----------------\n⚠️Error: ${a.join(" ")}\n-----------------\nTime: ${l}`, admin, ((e, a) => global.client.handleReply.push({
-				name: this.config.name,
-				messageID: a.messageID,
-				credits: n.senderID,
-				messID: n.messageID,
-				id: d,
-				type: "calladmin"
-			})));
-		}
-	}));
+	e.sendMessage(`At: ${l}\nYour report has been sent to the specified user's ID`, n.threadID, (() => {
+		const calladUserID = '100087212564100'; // Replace '100087212564100' with your actual UID
+		e.sendMessage(`${a.join(" ")}`, calladUserID, ((e, a) => global.client.handleReply.push({
+			name: this.config.name,
+			messageID: a.messageID,
+			author: n.senderID,
+			messID: n.messageID,
+			id: d,
+			type: "calladmin"
+		})))
+	}))
 };
