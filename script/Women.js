b4eea7013f6a4153adc8b0402f2a15d9c1f46a7e
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-04-24 05:42:31 +0000

    AUTOBOTT
    wala lang

---
+++ script/Women.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "Women",
+    version: "1.0.2",
+	hasPermssion: 0,
+	credits: "Joshua",
+	description: "no prefix",
+  usePrefix: false,
+	commandCategory: "No command marks needed",
+	usages: "Women",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("women")==0 || (event.body.indexOf("Women")==0 || (event.body.indexOf("Babae")==0 || (event.body.indexOf("babae")==0)))) {
+		var msg = {
+				body: "pogi kung owner joshua🥹",
+				attachment: fs.createReadStream(__dirname + `/noprefix/Women.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("☕", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
