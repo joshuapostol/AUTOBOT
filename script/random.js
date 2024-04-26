cef94c19a4845aa445e2842d381fc620d77df54f
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-04-26 18:49:24 +0800

    Create random.js

---
+++ script/random.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+const path = require("path");
+const fs = require("fs");
+ 
+module.exports.config = {
+    name: "random",
+    version: "1.0.0",
+    credits: "joshua Apoatol",
+    description: "Generate random TikTok girl edit videos",
+    hasPermssion: 0,
+    commandCategory: "other",
+    usages: "[RandomTiktok]",
+    cooldowns: 5,
+    dependencies: [],
+    usePrefix: true,
+};
+ 
+module.exports.run = async function ({ api, event }) {
+    try {
+ api.sendMessage(`video is sending please wait...`, event.threadID, event.messageID);
+ 
+        const response = await axios.post(`https://random-tiktok-video-api.onrender.com/api/request/f`);
+        const videoUrl = response.data.url;
+        const username = response.data.username;
+        const nickname = response.data.nickname;
+ 
+ 
+        const videoPath = path.resolve(__dirname, 'girledit_video.mp4');
+        const writer = fs.createWriteStream(videoPath);
+        const responseStream = await axios({
+            url: videoUrl,
+            method: 'GET',
+            responseType: 'stream'
+        });
+ 
+        responseStream.data.pipe(writer);
+ 
+        writer.on('finish', () => {
+            api.sendMessage({
+                body: `Username: ${username}\nNickname: ${nickname}`,
+                attachment: fs.createReadStream(videoPath)
+            }, event.threadID, () => fs.unlinkSync(videoPath), event.messageID);
+        });
+    } catch (error) {
+  api.sendMessage(`error fetching girl edit api!!\n${error}`, event.threadID, event.messageID);
+        console.error('Error:', error.message);
+    }
+};
+        
+      
