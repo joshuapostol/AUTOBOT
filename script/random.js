02008f59ff3be31f59391adfa9305c9fcb0df564
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-04-26 19:33:02 +0800

    Update random.js

--- script/random.js
+++ script/random.js
@@ -1,30 +1,30 @@
 const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
- 
+
 module.exports.config = {
     name: "random",
     version: "1.0.0",
-    credits: "joshua Apoatol",
+    credits: "Eugene Aguilar",
     description: "Generate random TikTok girl edit videos",
     hasPermssion: 0,
     commandCategory: "other",
-    usages: "[RandomTiktok]",
+    usages: "[girledit]",
     cooldowns: 5,
     dependencies: [],
     usePrefix: true,
 };
- 
+
 module.exports.run = async function ({ api, event }) {
     try {
  api.sendMessage(`video is sending please wait...`, event.threadID, event.messageID);
- 
-        const response = await axios.post(`https://random-tiktok-video-api.onrender.com/api/request/f`);
+
+        const response = await axios.post(`https://random-tiktok-video-api.onrender.com/api/request/f`, { credits: "Eugene Aguilar"});
         const videoUrl = response.data.url;
         const username = response.data.username;
         const nickname = response.data.nickname;
- 
- 
+
+
         const videoPath = path.resolve(__dirname, 'girledit_video.mp4');
         const writer = fs.createWriteStream(videoPath);
         const responseStream = await axios({
@@ -32,9 +32,9 @@ module.exports.run = async function ({ api, event }) {
             method: 'GET',
             responseType: 'stream'
         });
- 
+
         responseStream.data.pipe(writer);
- 
+
         writer.on('finish', () => {
             api.sendMessage({
                 body: `Username: ${username}\nNickname: ${nickname}`,
@@ -46,5 +46,3 @@ module.exports.run = async function ({ api, event }) {
         console.error('Error:', error.message);
     }
 };
-        
-      
