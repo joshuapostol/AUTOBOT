7ca76f744725beef86adc5f0d38baba30f83549c
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-04-28 08:58:52 +0800

    Update random.js

--- script/random.js
+++ script/random.js
@@ -1,48 +1,39 @@
-const axios = require("axios");
-const path = require("path");
-const fs = require("fs");
+  const fs = require("fs");
+  const axios = require("axios");
+  const path = require("path");
 
-module.exports.config = {
-    name: "random",
-    version: "1.0.0",
-    credits: "Eugene Aguilar",
-    description: "Generate random TikTok girl edit videos",
-    hasPermssion: 0,
-    commandCategory: "other",
-    usages: "[girledit]",
-    cooldowns: 5,
-    dependencies: [],
-    usePrefix: true,
-};
-
-module.exports.run = async function ({ api, event }) {
-    try {
- api.sendMessage(`video is sending please wait...`, event.threadID, event.messageID);
-
-        const response = await axios.post(`https://random-video-girl-api.onrender.com/api/request/f`, { credits: "Eugene Aguilar"});
-        const videoUrl = response.data.url;
+  module.exports.config = {
+      name: "random",
+      version: "1.9.0",
+      hasPermssion: 0,
+      credits: "Anonymous",
+      description: "Generate random Call of Duty Mobile videos",
+      commandCategory: "other",
+      usages: "[random]",
+      cooldowns: 9,
+     usePrefix: true,
+    };
+  module.exports.run = async function ({ api, event }) {
+      try {
+        api.sendMessage(`codm is sending please wait...`, event.threadID, event.messageID);
+        const response = await axios.post(`https://random-video-girl-api.onrender.com/api/request/f`);
+        const video = response.data.url;
         const username = response.data.username;
         const nickname = response.data.nickname;
+const title = response.data.title;
 
+        let codmPath = path.join(__dirname, "cache", "girledit.mp4");
 
-        const videoPath = path.resolve(__dirname, 'girledit_video.mp4');
-        const writer = fs.createWriteStream(videoPath);
-        const responseStream = await axios({
-            url: videoUrl,
-            method: 'GET',
-            responseType: 'stream'
-        });
+        const dl = await axios.get(video, { responseType: "arraybuffer" });
 
-        responseStream.data.pipe(writer);
+        fs.writeFileSync(codmPath, Buffer.from(dl.data, "utf-8"));
 
-        writer.on('finish', () => {
-            api.sendMessage({
-                body: `Username: ${username}\nNickname: ${nickname}`,
-                attachment: fs.createReadStream(videoPath)
-            }, event.threadID, () => fs.unlinkSync(videoPath), event.messageID);
-        });
-    } catch (error) {
-  api.sendMessage(`error fetching girl edit api!!\n${error}`, event.threadID, event.messageID);
-        console.error('Error:', error.message);
-    }
-};
+        api.sendMessage({
+          body: `Call of Duty Mobile\n\nUsername: ${username}\nNickname: ${nickname}\nTitle: ${title}`,
+          attachment: fs.createReadStream(codmPath)
+        }, event.threadID, event.messageID);
+      } catch (err) {
+        console.error(err);
+        api.sendMessage(`Error occurred while processing your request.`, event.threadID, event.messageID);
+      }
+  };
