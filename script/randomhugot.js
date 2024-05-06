6d84e1bcf9c68720fecb641b93193bf9693d43cd
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-07 05:56:27 +0800

    Create randomhugot.js

---
+++ script/randomhugot.js
@@ -0,0 +1,30 @@
+module.exports.config = {
+	name: "hugot",
+	version: "1.0.0",
+	hasPermission: 0,
+	credits: "joshua Apostol",
+	description: "Get a random hugot quote",
+	commandCategory: "fun",
+	cooldowns: 5,
+	dependencies: {
+		"axios": ""
+	}
+};
+
+const axios = require("axios");
+
+module.exports.run = async function({ api, event }) {
+	api.sendMessage("⏱️ | Fetching random hugot quote. Please wait...", event.threadID, event.messageID);
+  
+	const response = await axios.get('https://joshua09.pythonanywhere.com/hugot').catch(error => {
+		api.sendMessage("Error fetching hugot quote.", event.threadID, event.messageID);
+		console.error(error);
+		return;
+	});
+  
+	if (response && response.status === 200 && response.data && response.data.message) {
+		api.sendMessage(response.data.message, event.threadID);
+	} else {
+		api.sendMessage("Failed to retrieve a hugot quote.", event.threadID);
+	}
+};
