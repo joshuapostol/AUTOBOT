05ecaa7f657caca07ce51a700ce3477b9cc36bdb
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-07 06:20:48 +0800

    Update randomhugot.js

--- script/randomhugot.js
+++ script/randomhugot.js
@@ -3,26 +3,31 @@ const axios = require('axios');
 module.exports = {
     name: 'randomhugot',
     version: '1.0.0',
-    author: 'joshua Apostol',
+    author: 'Your Name',
     description: 'Get a random hugot quote',
     permissions: {
-        '*': '1'
+        '*': '2'
     },
     cooldown: 5,
     dependencies: {
         'axios': ''
     },
+    usePrefix: true,
 
-    execute: async function(args, api, event) {
+    execute: async function({ args, api, event }) {
         try {
-            api.sendMessage('⏱️ | Fetching random hugot quote. Please wait...', event.threadID, event.messageID);
+            if (args.length !== 0) {
+                api.sendMessage('⏱️ | Fetching random hugot quote. Please wait...', event.threadID, event.messageID);
 
-            const response = await axios.get('https://joshua09.pythonanywhere.com/hugot');
+                const response = await axios.get('https://joshua09.pythonanywhere.com/hugot');
 
-            if (response && response.status === 200 && response.data && response.data.message) {
-                api.sendMessage(response.data.message, event.threadID);
+                if (response && response.status === 200 && response.data && response.data.message) {
+                    api.sendMessage(response.data.message, event.threadID);
+                } else {
+                    api.sendMessage('Failed to retrieve a hugot quote.', event.threadID);
+                }
             } else {
-                api.sendMessage('Failed to retrieve a hugot quote.', event.threadID);
+                api.sendMessage('Invalid command usage. Usage: /randomhugot', event.threadID, event.messageID);
             }
         } catch (error) {
             console.error(error);
