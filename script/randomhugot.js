168e244f960b0c1e4a0c9ff9d539dbe1c1324d8b
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-07 06:06:55 +0800

    Update randomhugot.js

--- script/randomhugot.js
+++ script/randomhugot.js
@@ -1,30 +1,32 @@
-module.exports.config = {
-	name: "hugot",
-	version: "1.0.0",
-	hasPermission: 0,
-	credits: "joshua Apostol",
-	description: "Get a random hugot quote",
-	commandCategory: "fun",
-	cooldowns: 5,
-	dependencies: {
-		"axios": ""
-	}
-};
+const axios = require('axios');
 
-const axios = require("axios");
+module.exports = {
+    name: 'randomhugot',
+    version: '1.0.0',
+    author: 'joshua Apostol,
+    description: 'Get a random hugot quote',
+    permissions: {
+        '*': '2'
+    },
+    cooldown: 5,
+    dependencies: {
+        'axios': ''
+    },
 
-module.exports.run = async function({ api, event }) {
-	api.sendMessage("⏱️ | Fetching random hugot quote. Please wait...", event.threadID, event.messageID);
-  
-	const response = await axios.get('https://joshua09.pythonanywhere.com/hugot').catch(error => {
-		api.sendMessage("Error fetching hugot quote.", event.threadID, event.messageID);
-		console.error(error);
-		return;
-	});
-  
-	if (response && response.status === 200 && response.data && response.data.message) {
-		api.sendMessage(response.data.message, event.threadID);
-	} else {
-		api.sendMessage("Failed to retrieve a hugot quote.", event.threadID);
-	}
+    execute: async ({ api, event }) => {
+        try {
+            api.sendMessage('⏱️ | Fetching random hugot quote. Please wait...', event.threadID, event.messageID);
+
+            const response = await axios.get('https://joshua09.pythonanywhere.com/hugot');
+
+            if (response && response.status === 200 && response.data && response.data.message) {
+                api.sendMessage(response.data.message, event.threadID);
+            } else {
+                api.sendMessage('Failed to retrieve a hugot quote.', event.threadID);
+            }
+        } catch (error) {
+            console.error(error);
+            api.sendMessage('Error fetching hugot quote.', event.threadID);
+        }
+    }
 };
