2ad6888df5df1ac15ec89d3a47adf3c1070fafda
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-08 21:06:53 +0800

    Create snowflake.js

---
+++ script/snowflake.js
@@ -0,0 +1,52 @@
+module.exports.config = {
+	name: "snowflake",
+	version: "1.0.0",
+	hasPermission: 0,
+	hashPermission: 2,
+	credits: "joshua Apostol",
+	description: "Get response from snowflake API",
+	commandCategory: "fun",
+	cooldowns: 5,
+	hashPrefix: true,
+	dependencies: {
+		"axios": ""
+	}
+};
+
+const axios = require("axios");
+
+module.exports.run = async function({ api, event, args }) {
+	const input = args.join(" ");
+	if (!input) {
+		api.sendMessage("⚠️ Please provide a question.", event.threadID, event.messageID);
+		return;
+	}
+	
+	api.sendMessage("⏱️ | Fetching response. Please wait...", event.threadID, event.messageID);
+	
+	const response = await axios.get(`https://hashier-api-snowflake.vercel.app/api/snowflake?ask=${encodeURIComponent(input)}`).catch(error => {
+		api.sendMessage("Error fetching response.", event.threadID, event.messageID);
+		console.error(error);
+		return;
+	});
+	
+	if (response && response.status === 200 && response.data && response.data.answer) {
+		const answer = response.data.answer;
+		const question = input;
+		
+		const questionBox = "```" + `
+			💭 Question:
+			${question}
+		` + "```";
+		
+		const answerBox = "```" + `
+			💡 Answer:
+			${answer}
+		` + "```";
+		
+		api.sendMessage(questionBox, event.threadID);
+		api.sendMessage(answerBox, event.threadID);
+	} else {
+		api.sendMessage("Failed to get a response.", event.threadID);
+	}
+};
