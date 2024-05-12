e95a8e0c38168eda7ec7baffdbcbea9ac32b33fd
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-12 21:27:00 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,27 +1,44 @@
 module.exports.config = {
-  name: "ai",
-  version: "30.0.0",
-  hasPermssion: 0,
-  credits: "Choru TikTokers",
-  description: "ChatGpt",
-  commandCategory: "Other",
-  usages: "query",
-  hasPrefix: false,
-  cooldowns: 5
+		name: "ai",
+		version: "1.0.0",
+		role: 0,
+		credits: "cliff",
+		hasPrefix: false,
+		description: "This module provides AI-powered responses using GPT-3.",
+		usage: "<question>",
+		cooldowns: 5,
+	usePrefix: false,
+	  hasPermission: 0,
+	  usages: "<question>",
+		cooldown: 5,
+	 commandCategory: "gpt4",
+		aliases: ["Gpt3"]
 };
 
-module.exports.run = async ({ api, event, args }) => { 
-  const { gpt } = require("gpti");
-  let query = args.join(" ");
-  gpt.v1({
-      prompt: query,
-      model: "GPT-4",
-      markdown: false
-  }, (err, data) => {
-      if(err != null){
-          console.log(err);
-      } else {
-          api.sendMessage(data.gpt, event.threadID, event.messageID);
-      }
-  });
-};
+module.exports.run = async function ({ api, event, args, _GLOBAL, client}) {
+		try {
+				const { G4F } = require("g4f");
+
+				function reply(a) {
+						api.sendMessage(a, event.threadID, event.messageID);
+				}
+
+				const g4f = new G4F();
+				const textInput = args.join(' ');
+				if (!textInput) return reply('mag provide ka naman ng question tanginamo ka parang dika nag grade 2.');
+
+				const messages = [
+						{ role: "user", content: textInput }
+				];
+				const options = {
+						provider: g4f.providers.GPT,
+						model: "gpt-3.5-turbo",
+						debug: true,
+						proxy: `${global.config.UPDATE}`
+				};
+				const response = await g4f.chatCompletion(messages, options);
+				reply(response);
+		} catch (e) {
+				return reply(e.message);
+		}
+}
