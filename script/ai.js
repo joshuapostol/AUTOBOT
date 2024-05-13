7e40ccc82a48cf7684001a0dccc2255f481f0188
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-05-13 15:29:37 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -7,7 +7,7 @@ module.exports.config = {
   hasPermssion: 0,
   credits: 'Yan Maglinte | Liane Cagara',
   description: 'An AI command using Hercai API!',
-  usePrefix: false,
+  hasPrefix: false,
   allowPrefix: true,
   commandCategory: 'chatbots',
   usages: 'Ai [prompt]',
